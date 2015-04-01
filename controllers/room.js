033864bfce0dd96de8e43a2b92c910754e6926ae
Surdu Nicolae <nicolae.d.surdu@gmail.com>
2015-04-02 01:34:11 +0300

    More work to create/join room

--- controllers/room.js
+++ controllers/room.js
@@ -1,4 +1,5 @@
 var plug = require("../plug");
+var _ = require("underscore");
 
 var rooms = [];
 var lastRoom = 0;
@@ -15,6 +16,10 @@ Room.prototype = {
   }
 }
 
+function User(name) {
+  this.name = name;
+}
+
 plug.whenPlugged(function (socket) {
   socket.on("createRoom", function (data) {
 
@@ -25,11 +30,29 @@ plug.whenPlugged(function (socket) {
     socket.join(room.id);
 
     // store room id for easy identification later on
-    socket.pokerInfo = {
-      room: room
-    }
+    // socket.pokerInfo = {
+    //   room: room
+    // };
 
     socket.emit("roomCreated", {room: room})
+  });
+
+  socket.on("joinRoom", function (data) {
+    var room = _.find(rooms, function (room) {
+      return room.id == data.roomId;
+    });
+
+    if (!room) {
+      // bitch about it
+      return;
+    }
+
+    var user = new User(data.name);
+
+    // socket.pokerInfo.user = user;
+    room.users.push(user);
+
+    console.log(room);
   });
 });
 
--- static/js/room.js
+++ static/js/room.js
@@ -1,4 +1,14 @@
 (function () {
+
+  var socket = io();
+
+  function getParam(name) {
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+          results = regex.exec(location.search);
+      return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
   function adjustTableHeight(){
     $("#table").height($(window).height() - $("#hand").height() - 72);
   }
@@ -15,7 +25,9 @@
 
     $("#joinRoomForm").submit(function () {
       var name = $("#joinRoomForm #name").val();
-    
+      var roomId = getParam("id");
+      socket.emit("joinRoom", {roomId: roomId, name: name});
+
       return false;
     });
   });
