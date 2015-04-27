95665c3f706c99a68678e7c5ad2ec7cfcceeb01f
Surdu Nicolae <nicolae.d.surdu@gmail.com>
2015-04-28 00:11:51 +0300

    Implemented participants updating. Closes #7

--- static/js/room.js
+++ static/js/room.js
@@ -3,11 +3,45 @@
 
   var socket = io();
   var room, roomId;
+  var playingTable = $("#table");
+  var users = [];
 
   // attempt to extract room id from URL hash
   var hashMatch = window.location.hash.match(/^#\/view\/(\d+)$/);
   if (hashMatch) {
     roomId = hashMatch[1];
+  }
+
+  function addUser(userJoining) {
+    userJoining.playerDOM = $(nj.render("player.html", userJoining));
+    users.push(userJoining)
+    playingTable.append(userJoining.playerDOM);
+  }
+
+  function removeUser(userLeaving) {
+    var user = _.find(users, function (user) {
+      return user.id == userLeaving.id;
+    });
+
+    user.playerDOM.remove();
+  }
+
+
+  function handleJoinRoom(roomInfo) {
+    room = roomInfo.room;
+    $('#nameModal').modal('hide');
+
+    if (!roomId) {
+      window.location = "#/view/" + room.id;
+    }
+
+    for (var f = 0 ; f < room.users.length; f++) {
+      var user = room.users[f];
+      addUser(user);
+    }
+
+    socket.on("userJoined", addUser);
+    socket.on("userLeft", removeUser);
   }
 
   // create the modal
@@ -17,26 +51,7 @@
   $("#joinRoomForm").submit(function () {
     var name = $("#joinRoomForm #name").val();
 
-    socket.on("roomJoined", function (data) {
-      room = data.room;
-      $('#nameModal').modal('hide');
-
-      if (!roomId) {
-        window.location = "#/view/" + room.id;
-      }
-
-      var tableEl = $("#table");
-
-      for (var f = 0 ; f < room.users.length; f++) {
-        var user = room.users[f];
-        user.cardValue = 1;
-        tableEl.append(nj.render("player.html", user))
-      }
-
-      $("#table .card").click(function(){
-        $(this).toggleClass("turned")
-      });
-    });
+    socket.on("roomJoined", handleJoinRoom);
 
     socket.emit("joinRoom", {roomId: roomId, name: name});
 
